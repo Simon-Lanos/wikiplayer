692991e9e9240ecaee6953b28f88c54cb3002683
Simon LANOS <simon.lanos99@gmail.com>
2021-07-29 22:16:02 +0200

    feat(home): add assets and fix favicon

---
+++ public/assets/js/home.js
@@ -0,0 +1,44 @@
+Circles.create({
+        id:           'circle-1',
+        radius:       60,
+        value:        87,
+        maxValue:     100,
+        width:        18,
+        text:         '',
+        colors:       ['#FFF', '#5902E5'],
+        duration:     400,
+        wrpClass:     'circles-wrp',
+        textClass:    'circles-text',
+        styleWrapper: true,
+        styleText:    true
+});
+
+Circles.create({
+        id:           'circle-2',
+        radius:       60,
+        value:        58,
+        maxValue:     100,
+        width:        18,
+        text:         '',
+        colors:       ['#FFF', '#5902E5'],
+        duration:     400,
+        wrpClass:     'circles-wrp',
+        textClass:    'circles-text',
+        styleWrapper: true,
+        styleText:    true
+});
+
+Circles.create({
+        id:           'circle-3',
+        radius:       60,
+        value:        38,
+        maxValue:     100,
+        width:        18,
+        text:         '',
+        colors:       ['#FFF', '#5902E5'],
+        duration:     400,
+        wrpClass:     'circles-wrp',
+        textClass:    'circles-text',
+        styleWrapper: true,
+        styleText:    true
+});
---
+++ public/assets/js/timer.js
@@ -0,0 +1,14 @@
+const timer = document.querySelector('#timer');
+let time = (new Date("Oct 1, 2021 00:00:00").getTime() - new Date()) / 1000; //1837879 //seconds until dawn
+
+setInterval(
+    () => {
+        time--;
+        let d = Math.floor(time / (3600*24));
+        let h = Math.floor(time % (3600*24) / 3600);
+        let m = Math.floor(time % 3600 / 60);
+        let s = Math.floor(time % 60);
+        timer.textContent = `${d}j ${h}h ${m}m ${s}s`;
+    },
+    1000
+)
